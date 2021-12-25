5028d791fd67f060ac7e6df81eb53f4ab640be90
Noushedul1 <noushedakib@gmail.com>
2021-12-25 13:56:48 +0600

    create a vue form and add some form validation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <form @submit.prevent="submithandle">
+      <div>
+        <label>First name : </label>
+        <input type="text" v-model.lazy="formData.firstname">
+      </div>
+      <div>
+        <label>Last name : </label>
+        <input type="text" v-model.lazy="formData.lastname">
+      </div>
+      <div>
+        <label>Password: </label>
+        <input type="text" v-model.lazy="formData.password">
+        <p v-if="formData.errpass">{{formData.errpass}}</p>
+      </div>
+      <div>
+        <label>Position :</label>
+        <select v-model="formData.position">
+          <option>Web Design</option>
+          <option>Graphic Design</option>
+          <option>UI/UX Designer</option>
+          <option>Web Development</option>
+          <option>Excel</option>
+        </select>
+      </div>
+      <div>
+        <label>Skill :</label>
+        <input type="text" v-model="formData.
+        tempSkill" @keyup.alt="press">
+        <div v-for="skill in formData.skills" :key="skill" class="pill">
+          <span @click="deleteSkill(skill)">{{skill}}</span>
+        </div>
+      </div>
+      <div>
+        <input type="radio" v-model="formData.terms">
+        <label>I agree with this rule.</label>
+      </div>
+      <input type="submit" value="Submit">
+    </form>
+    <div>
+    <p>First name : {{formData.firstname}}</p>
+    <p>Last name : {{formData.lastname}}</p>
+    <p>Password name : {{formData.password}}</p>
+    <p>Terms : {{formData.terms}}</p>
+    <p>Position : {{formData.position}}</p>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        formData: {
+          firstname: '',
+          lastname: '',
+          password: '',
+          terms: false,
+          position: '',
+          tempSkill: '',
+          skills: [],
+          errpass: ''
+        }
+      }
+    },
+    methods: {
+      press(e) {
+        if(e.key === ',' && this.formData.tempSkill){
+          if(!this.formData.skills.includes(this.formData.tempSkill)){
+             this.formData.skills.push(this.formData.tempSkill);
+          }
+          this.formData.tempSkill = '';
+        }
+      },
+      deleteSkill(skill){
+        this.formData.skills = this.formData.skills.filter((item)=>{
+          return skill !== item;
+        })
+      },
+      submithandle() {
+        this.formData.errpass = (this.formData.password.length > 6) ? '' : 'password is more than 6 char. ';
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.pill{
+  background: #ddd;
+  padding: 10px;
+  width: 100px;
+  margin: 10px;
+  border-radius: 10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
